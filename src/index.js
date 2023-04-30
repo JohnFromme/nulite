1c40c2fbf16bd252a1899f1b47079e7a723833d2
John Fromme <john@johnfromme.com>
2023-04-30 15:34:21 -0700

    Made changes to README & src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import chalk from 'chalk';
 import gradient from 'gradient-string';
-// import inquirer from 'inquirer';
+// import inquirer from 'inquirer'; 
 
 console.log(chalk.bgBlack(gradient.morning(' ------------------------------------------ ')));
 console.log(chalk.bgBlack(gradient.morning(' Nulite: Your AI powered programming buddy! ')));
